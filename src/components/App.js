b0d030d4ae3e696711b435dcc3dbc512e58b7d29
Robert <robert.dolibog@outlook.de>
2024-06-17 23:56:52 +0200

    more file route changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import BackgroundVideo from './components/BackgroundVideo.js';
-import ProjectList from './components/ProjectList.js';
-import CVLink from './components/CVLink.js';
+import BackgroundVideo from './BackgroundVideo';
+import ProjectList from './ProjectList';
+import CVLink from './CVLink';
 import './css/App.css';
 
 const App = () => {

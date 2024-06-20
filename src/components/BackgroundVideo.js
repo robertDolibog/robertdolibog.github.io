0f0b2196edc41450ef6b1eda61b1ab2ca7e0b3c8
Robert <robert.dolibog@outlook.de>
2024-06-20 20:35:00 +0200

    changed video overlay and wideness. to match projects

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -3,11 +3,12 @@ import '../css/BackgroundVideo.css';
 
 const BackgroundVideo = () => {
   return (
-    <div className="background-video">
-      <video autoPlay loop muted>
-      <source src={`${process.env.PUBLIC_URL}/video_quickshot_cropped.mp4`} type="video/mp4" />
+    <div className="background-video-container">
+      <video className="background-video" autoPlay loop muted>
+        <source src={`${process.env.PUBLIC_URL}/videos/background.mp4`} type="video/mp4" />
+        Your browser does not support the video tag.
       </video>
-      <div className="overlay">
+      <div className="video-overlay">
         <h1>Welcome to My Personal Website</h1>
       </div>
     </div>

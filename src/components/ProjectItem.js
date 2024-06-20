66e29ef5c32e440ebbb85d8ada6bef0ee17ce3dd
Robert <robert.dolibog@outlook.de>
2024-06-20 18:00:33 +0200

    new design for projects

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import '../css/ProjectItem.css';
+import './css/ProjectItem.css';
 
-const ProjectItem = ({ project }) => {
+const ProjectItem = ({ image, title, description, link }) => {
   return (
     <div className="project-item">
-      <h3>{project.title}</h3>
-      <p>{project.description}</p>
-      <a href={project.link} target="_blank" rel="noopener noreferrer">Learn more</a>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <a href={link} target="_blank" rel="noopener noreferrer">{title}</a>
     </div>
   );
 };
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import ProjectItem from './ProjectItem';
-import '../css/ProjectList.css';
-
-const projects = [
-  {
-    title: 'AvatAVR',
-    description: 'An innovative Unity-based VR game showcased at HTW Berlin.',
-    link: 'https://showtime.f4.htw-berlin.de/ws23/bachelor/b6-avatavr/'
-  },
-  {
-    title: 'Another Project',
-    description: 'Description of another project.',
-    link: '#'
-  }
-  // Add more projects as needed
-];
+import './css/ProjectList.css';
 
 const ProjectList = () => {
   return (
     <div className="project-list">
-      {projects.map((project, index) => (
-        <ProjectItem key={index} project={project} />
-      ))}
+      <ProjectItem
+        image={`${process.env.PUBLIC_URL}/images/AvataVr.png`}
+        title="AvatAVR"
+        description="An innovative Unity-based VR game showcased at HTW Berlin."
+        link="https://showtime.f4.htw-berlin.de/ws23/bachelor/b6-avatavr/"
+      />
+      <ProjectItem
+        image="path/to/your/image2.jpg"
+        title="Another Project"
+        description="Description of another project."
+        link="https://example.com"
+      />
     </div>
   );
 };

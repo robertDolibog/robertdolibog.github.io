7c3c139a864d48ca1f0a895fd6aa04a61b6bd59a
Robert <robert.dolibog@outlook.de>
2024-10-04 16:00:58 +0200

    added AvataVR Documentation

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ProjectItem from './ProjectItem';
 import '../css/ProjectList.css';
 
-
 const ProjectList = () => {
   return (
     <div className="project-list">
@@ -12,11 +11,12 @@ const ProjectList = () => {
         description="An innovative Unity-based VR game showcased at HTW Berlin."
         role="My role involved implementing a GitFlow-like workflow in team development and establishing a CI pipeline on GitLab to ensure the Unity game would build for every push to our version control."
         link="https://showtime.f4.htw-berlin.de/ws23/bachelor/b6-avatavr/"
+        docLink={`${process.env.PUBLIC_URL}/Project_DevOps.pdf`}
       />
       <ProjectItem
         image={`${process.env.PUBLIC_URL}/images/ProjectmanagementPro.png`}
         title="Project Management Pro (Ongoing)"
-        description="A project management tool developed using Next.js and and Express.js."
+        description="A project management tool developed using Next.js and Express.js."
         role="My role involved developing the backend API via the Express.js framework and our Postgres Database. Further I was orchestrating the frontend connection via Docker-Compose."
         link="https://github.com/robertDolibog/ProjectManagementPro"
       />
